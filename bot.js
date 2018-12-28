0fec5964090f204506f7e18d13269cc60f8c5085
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-12-28 13:57:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
   fs.writeFile("./json/awsmm.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   })
-});*/
+});
 
 
 
